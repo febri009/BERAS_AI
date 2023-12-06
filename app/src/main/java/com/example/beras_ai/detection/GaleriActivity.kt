858865f7c5d0c87372c6560e2e2059b28af68793
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-12-06 21:46:37 +0700

    add GaleriActivity and slicing activity_galeri.xml layout

---
+++ app/src/main/java/com/example/beras_ai/detection/GaleriActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beras_ai.detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beras_ai.R
+
+class GaleriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_galeri)
+    }
+}
