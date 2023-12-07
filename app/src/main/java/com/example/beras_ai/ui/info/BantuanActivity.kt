4285357d5d3de2f4c443763ec0dee4e4891dc5c8
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-12-07 12:49:12 +0700

    Slicing activity_bantuan.xml layout and Release v1.8

--- app/src/main/java/com/example/beras_ai/ui/info/BantuanActivity.kt
+++ app/src/main/java/com/example/beras_ai/ui/info/BantuanActivity.kt
@@ -2,11 +2,22 @@ package com.example.beras_ai.ui.info
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import com.example.beras_ai.R
 
 class BantuanActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bantuan)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = " "
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
