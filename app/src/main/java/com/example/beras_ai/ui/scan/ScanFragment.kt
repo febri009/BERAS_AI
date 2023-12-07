d589fbb45415f1e8e260386d24a9c405fc6f517e
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-12-07 13:29:45 +0700

    Finishing app (Add app icon and remove extra margin) and Release v1.9

--- app/src/main/java/com/example/beras_ai/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/beras_ai/ui/scan/ScanFragment.kt
@@ -1,11 +1,14 @@
 package com.example.beras_ai.ui.scan
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.beras_ai.databinding.FragmentScanBinding
+import com.example.beras_ai.detection.GaleriActivity
+import com.example.beras_ai.detection.KameraActivity
 
 class ScanFragment : Fragment() {
 
@@ -15,6 +18,16 @@ class ScanFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        binding.buttonCamera.setOnClickListener {
+            val intent = Intent(requireContext(), KameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.buttonGallery.setOnClickListener {
+            val intent = Intent(requireContext(), GaleriActivity::class.java)
+            startActivity(intent)
+        }
 
         return root
     }
