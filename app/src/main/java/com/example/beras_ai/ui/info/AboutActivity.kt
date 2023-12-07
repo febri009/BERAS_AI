75234d30739cfc55a2cc72ac87950ed93a995bd6
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-12-07 11:39:05 +0700

    Add AboutActivity and BantuanActivity

---
+++ app/src/main/java/com/example/beras_ai/ui/info/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beras_ai.ui.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beras_ai.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
---
+++ app/src/main/java/com/example/beras_ai/ui/info/BantuanActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beras_ai.ui.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beras_ai.R
+
+class BantuanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bantuan)
+    }
+}
