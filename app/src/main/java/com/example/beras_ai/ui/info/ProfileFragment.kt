a9b9ede0df6036f4e8d14f23d8b26f53668bc0e4
Febriyanti Paramudita <febriyantiparamudita8@gmail.com>
2023-12-07 11:43:44 +0700

    - Set a click event on the "About" and "Bantuan" card for intent to AboutActivity and BantuanActivity.  - Release v1.6

--- app/src/main/java/com/example/beras_ai/ui/info/ProfileFragment.kt
+++ app/src/main/java/com/example/beras_ai/ui/info/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.beras_ai.ui.info
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -19,8 +20,20 @@ class ProfileFragment : Fragment() {
     ): View {
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
 
-        return binding.root
+        // Mengatur event click pada card "Bantuan"
+        binding.buttonBantuan.setOnClickListener {
+            val intent = Intent(requireContext(), BantuanActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Mengatur event click pada card "About"
+        binding.buttonAbout.setOnClickListener {
+            val intent = Intent(requireContext(), AboutActivity::class.java)
+            startActivity(intent)
+        }
+        return root
     }
 
     override fun onDestroyView() {
